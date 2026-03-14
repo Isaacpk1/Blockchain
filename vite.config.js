1fd14d6f502036037cbb585bd1bec6c8d03ffc1a
Isaacpk1 <isaacnicolasalvesdasilva@gmail.com>
2026-03-14 20:31:35 -0300

    Change default repo name to 'Blockchain'

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // Para GitHub Pages: use o nome do repositório. Ex.: repo "blockchain" → base: '/Blockchain/'
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'blockchain'
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'Blockchain'
 
 export default defineConfig({
   base: `/${repoName}/`,
